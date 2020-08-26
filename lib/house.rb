529963bd16740150e2259b3f23bf8c3f343eba28
Nick King <nickmaxking@gmail.com>
2020-08-26 09:53:17 -0600

    Add rooms_from_category method

--- lib/house.rb
+++ lib/house.rb
@@ -10,4 +10,17 @@ class House
   def add_room(room)
     @rooms << room
   end
+
+  def above_market_average?
+    integer_price = @price[1..-1]
+    integer_price.to_i > 500000
+  end
+
+  def rooms_from_category(type)
+    rooms_in_category = @rooms.map do |room|
+      if room.category == type
+        room
+      end
+    end
+  end
 end
