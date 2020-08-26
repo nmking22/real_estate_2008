9f30407f1cde65510207323e5cde626c8d142297
Nick King <nickmaxking@gmail.com>
2020-08-26 10:12:18 -0600

    Add tests for area, details and price_per_square_foot

--- test/house_test.rb
+++ test/house_test.rb
@@ -50,4 +50,40 @@ class HouseTest < Minitest::Test
     assert_equal [room_4], house.rooms_from_category(:basement)
   end
 
+  def test_it_can_return_area
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal 1900, house.area
+  end
+
+  def test_it_can_check_details
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_instance_of Hash, house.details
+    assert_equal house.price, house.details["price"]
+    assert_equal house.address, house.details["address"]
+  end
+
+  def test_it_can_calculate_price_per_square_foot
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_4)
+    house.add_room(room_1)
+    house.add_room(room_3)
+    house.add_room(room_2)
+
+    assert_equal 210.53, house.test_it_can_calculate_price_per_square_foot
+  end
+
 end
